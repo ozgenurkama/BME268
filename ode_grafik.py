8e07fd4b1e843a5fc0bf3fc64e2bc93f2c66d4cc
ozgenurkama <ozgenurkama75@gmail.com>
2026-05-18 11:16:21 +0300

    Add files via upload

---
+++ ode_grafik.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+def pharmacokinetics(C, t, k):
+    return -k * C
+
+
+C0 = 10.0
+k = 0.3
+t = np.linspace(0, 24, 200)
+
+C_num = odeint(pharmacokinetics, C0, t, args=(k,))
+C_exact = C0 * np.exp(-k * t)
+
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.plot(t, C_num[:, 0], "b-", lw=2, label="odeint (numerical)")
+ax.plot(t, C_exact, "r--", lw=2, label="analytical")
+ax.set_xlabel("Time (hours)")
+ax.set_ylabel("Concentration (mg/L)")
+ax.set_title("Drug Elimination - Numerical vs. Analytical")
+ax.legend()
+ax.grid(True, alpha=0.3)
+
+plt.show()
